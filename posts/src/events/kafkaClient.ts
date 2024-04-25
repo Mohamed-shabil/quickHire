79bac7abf12151a626de6d8f3eb9661888662b5d
Mohamed Shabil <mohamedshabil231@gmail.com>
2024-04-25 11:39:27 +0530

    refactor(posts): add kafka url to env variables

--- posts/src/events/kafkaClient.ts
+++ posts/src/events/kafkaClient.ts
@@ -1,8 +1,10 @@
 import { Kafka } from "kafkajs";
 
+console.log("Kafka is here ", process.env.KAFKA_SERVICE);
+
 export const kafkaClient = new Kafka({
     clientId: "profile-service",
-    brokers: ["kafka-service:9092"],
+    brokers: [process.env.KAFKA_SERVICE!],
     retry: {
         initialRetryTime: 100,
         retries: 8,
