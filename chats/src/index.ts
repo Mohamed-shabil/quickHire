334e3d78211b69049cdd6290ae1905d790173556
Mohamed Shabil <mohamedshabil231@gmail.com>
2024-05-01 11:00:46 +0530

    typo(chats): uncomment kafka retry mechanism

--- chats/src/index.ts
+++ chats/src/index.ts
@@ -75,28 +75,28 @@ const start = async () => {
 
 start();
 
-// const errorTypes = ["unhandledRejection", "uncaughtException"];
-// const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2"];
+const errorTypes = ["unhandledRejection", "uncaughtException"];
+const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2"];
 
-// errorTypes.forEach((type) => {
-//     process.on(type, async (e) => {
-//         try {
-//             console.log(`process.on ${type}`);
-//             console.log(e);
-//             await consumer.disconnect();
-//             process.exit(0);
-//         } catch (error) {
-//             process.exit(1);
-//         }
-//     });
-// });
+errorTypes.forEach((type) => {
+    process.on(type, async (e) => {
+        try {
+            console.log(`process.on ${type}`);
+            console.log(e);
+            await consumer.disconnect();
+            process.exit(0);
+        } catch (error) {
+            process.exit(1);
+        }
+    });
+});
 
-// signalTraps.forEach((type) => {
-//     process.once(type, async () => {
-//         try {
-//             await consumer.disconnect();
-//         } catch (error) {
-//             process.kill(process.pid, type);
-//         }
-//     });
-// });
+signalTraps.forEach((type) => {
+    process.once(type, async () => {
+        try {
+            await consumer.disconnect();
+        } catch (error) {
+            process.kill(process.pid, type);
+        }
+    });
+});
